4ac60e3e2268da6b0cea95f7428ccfc7e222c598
fury473 <fury473@gmail.com>
2015-01-16 23:06:33 +0100

    Scroll to top button

--- src/RTG/AppBundle/Resources/public/js/main.js
+++ src/RTG/AppBundle/Resources/public/js/main.js
@@ -1,3 +1,31 @@
+function setScrollBtnOffset() {
+    var container = $('body > .container');
+    var container_width = container.width() + 30;
+    var offset = container.offset().left + container_width + 15;
+    return $('.scrollup').css('left', offset+'px');
+}
+
 $(function() {
     $('#carousel-example-generic').carousel();
+    
+    setScrollBtnOffset();
+    
+    $(window).resize(function() {
+        setScrollBtnOffset();
+    });
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
 });
